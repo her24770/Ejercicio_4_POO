013b43b6b3b1885f0b71b144f76ce934437990e8
Juan Daniel Ordoñez <ord24979@uvg.edu.gt>
2024-09-30 18:46:05 -0600

    Creeacion de clases Animal y Reptil

--- src/clases/Animal.java
+++ src/clases/Animal.java
@@ -1,4 +1,46 @@
 package clases;
 
 public class Animal {
+    private String nombreCientifico;
+    private String descripcionHabitat;
+    private int vidaEsperanza;
+
+    public Animal(String nombreCientifico, String descripcionHabitat, int vidaEsperanza){
+        this.nombreCientifico = nombreCientifico;
+        this.descripcionHabitat = descripcionHabitat;
+        this.vidaEsperanza = vidaEsperanza;
+    }
+
+    public Animal(){
+
+    }
+
+    public void setNombreCientifico(String nombreCientifico){
+        this.nombreCientifico = nombreCientifico;
+    }
+
+    public String getNombreCientifico(){
+        return nombreCientifico;
+    }
+
+    public void setDescripcionHabitat(String descripcionHabitat){
+        this.descripcionHabitat = descripcionHabitat;
+    }
+    public String getDescripcionHabitat(){
+        return descripcionHabitat;
+    }
+
+    public void setVidaEsperanza(int vidaEsperanza){
+        this.vidaEsperanza = vidaEsperanza;
+    }
+    public int getVidaEsperanza(){
+        return vidaEsperanza;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre Cientifico: " + nombreCientifico +
+                "\nDescripcion del Habitat: " + descripcionHabitat +
+                "\nEsperanza de Vida: " + vidaEsperanza;
+    }
 }
---
+++ src/clases/Reptil.java
@@ -0,0 +1,71 @@
+package clases;
+
+public class Reptil extends Animal{
+    private int temperatura;
+    private  double huevos;
+    private  double peso;
+    private  int longevidad;
+    private  boolean peligroExt;
+    private  String dieta;
+
+    public Reptil(int temperatura, double huevos, double peso, int longevidad, boolean peligroExt, String dieta){
+        this.temperatura = temperatura;
+        this.huevos = huevos;
+        this.peso = peso;
+        this.longevidad = longevidad;
+        this.peligroExt = peligroExt;
+        this.dieta = dieta;
+    }
+
+    public Reptil(){
+
+    }
+
+    public void setTemperatura(int temperatura){
+        this.temperatura = temperatura;
+    }
+
+    public int getTemperatura(){
+        return temperatura;
+    }
+
+    public void setHuevos(double huevos){
+        this.huevos = huevos;
+    }
+    public double getHuevos(){
+        return huevos;
+    }
+
+    public void setPeso(double peso){
+        this.peso = peso;
+    }
+    public double getPeso(){
+        return peso;
+    }
+
+    public void setLongevidad(int longevidad){
+        this.longevidad = longevidad;
+    }
+    public int getLongevidad(){
+        return longevidad;
+    }
+
+    public void setPeligroExt(boolean peligroExt){
+        this.peligroExt = peligroExt;
+    }
+    public boolean getPeligroExt(){
+        return peligroExt;
+    }
+
+    public void setDieta(String dieta){
+        this.dieta = dieta;
+    }
+    public String getDieta(){
+        return dieta;
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+}
