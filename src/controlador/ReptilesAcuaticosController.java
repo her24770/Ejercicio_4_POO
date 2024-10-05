cdf88f3dbfe0ad83e30969ec77a2e1558d971455
Gabrexcitado <gabrex08@gmail.com>
2024-10-04 18:25:29 -0600

    Arreglos en funcion presupuesto

--- src/controlador/ReptilesAcuaticosController.java
+++ src/controlador/ReptilesAcuaticosController.java
@@ -173,7 +173,7 @@ public class ReptilesAcuaticosController {
         }
         return msg;
     }
-    @Override
+
     public double[] presupuesto(ReptilAcuatico newReptilAcuatico) {
         double costoMenor;
         double costoMayor;
